317acd87bfc8a0661317e35cf2cbad1c850a7506
Thomas Freiherr <Thomas.Freiherr@gmx.net>
2018-08-06 19:59:24 +0200

    added: python IMPORTS
    Signed-off-by: Thomas Freiherr <Thomas.Freiherr@gmx.net>

--- src/python/libpruio/pruio.py
+++ src/python/libpruio/pruio.py
@@ -6,508 +6,340 @@ _libs = {}
 _libs['pruio'] = load_library('pruio')
 
 # pruio_pins.bi: 19
-try:
-  P8_03 = 0x06
-except:
-  pass
+try: P8_03 = 0x06
+except: pass
 
 # pruio_pins.bi: 21
-try:
-  P8_04 = 0x07
-except:
-  pass
+try: P8_04 = 0x07
+except: pass
 
 # pruio_pins.bi: 23
-try:
-  P8_05 = 0x02
-except:
-  pass
+try: P8_05 = 0x02
+except: pass
 
 # pruio_pins.bi: 25
-try:
-  P8_06 = 0x03
-except:
-  pass
+try: P8_06 = 0x03
+except: pass
 
 # pruio_pins.bi: 27
-try:
-  P8_07 = 0x24
-except:
-  pass
+try: P8_07 = 0x24
+except: pass
 
 # pruio_pins.bi: 29
-try:
-  P8_08 = 0x25 
-except:
-  pass
+try: P8_08 = 0x25 
+except: pass
 
 # pruio_pins.bi: 31
-try:
-  P8_09 = 0x27 
-except:
-  pass
+try: P8_09 = 0x27 
+except: pass
 
 # pruio_pins.bi: 33
-try:
-  P8_10 = 0x26 
-except:
-  pass
+try: P8_10 = 0x26 
+except: pass
 
 # pruio_pins.bi: 35
-try:
-  P8_11 = 0x0D 
-except:
-  pass
+try: P8_11 = 0x0D 
+except: pass
 
 # pruio_pins.bi: 37
-try:
-  P8_12 = 0x0C 
-except:
-  pass
+try: P8_12 = 0x0C 
+except: pass
 
 # pruio_pins.bi: 39
-try:
-  P8_13 = 0x09 
-except:
-  pass
+try: P8_13 = 0x09 
+except: pass
 
 # pruio_pins.bi: 41
-try:
-  P8_14 = 0x0A 
-except:
-  pass
+try: P8_14 = 0x0A 
+except: pass
 
 # pruio_pins.bi: 43
-try:
-  P8_15 = 0x0F 
-except:
-  pass
+try: P8_15 = 0x0F 
+except: pass
 
 # pruio_pins.bi: 45
-try:
-  P8_16 = 0x0E 
-except:
-  pass
+try: P8_16 = 0x0E 
+except: pass
 
 # pruio_pins.bi: 47
-try:
-  P8_17 = 0x0B 
-except:
-  pass
+try: P8_17 = 0x0B 
+except: pass
 
 # pruio_pins.bi: 49
-try:
-  P8_18 = 0x23 
-except:
-  pass
+try: P8_18 = 0x23 
+except: pass
 
 # pruio_pins.bi: 51
-try:
-  P8_19 = 0x08 
-except:
-  pass
+try: P8_19 = 0x08 
+except: pass
 
 # pruio_pins.bi: 53
-try:
-  P8_20 = 0x21 
-except:
-  pass
+try: P8_20 = 0x21 
+except: pass
 
 # pruio_pins.bi: 55
-try:
-  P8_21 = 0x20 
-except:
-  pass
+try: P8_21 = 0x20 
+except: pass
 
 # pruio_pins.bi: 57
-try:
-  P8_22 = 0x05 
-except:
-  pass
+try: P8_22 = 0x05 
+except: pass
 
 # pruio_pins.bi: 59
-try:
-  P8_23 = 0x04 
-except:
-  pass
+try: P8_23 = 0x04 
+except: pass
 
 # pruio_pins.bi: 61
-try:
-  P8_24 = 0x01 
-except:
-  pass
+try: P8_24 = 0x01 
+except: pass
 
 # pruio_pins.bi: 63
-try:
-  P8_25 = 0x00 
-except:
-  pass
+try: P8_25 = 0x00 
+except: pass
 
 # pruio_pins.bi: 65
-try:
-  P8_26 = 0x1F 
-except:
-  pass
+try: P8_26 = 0x1F 
+except: pass
 
 # pruio_pins.bi: 67
-try:
-  P8_27 = 0x38 
-except:
-  pass
+try: P8_27 = 0x38 
+except: pass
 
 # pruio_pins.bi: 69
-try:
-  P8_28 = 0x3A 
-except:
-  pass
+try: P8_28 = 0x3A 
+except: pass
 
 # pruio_pins.bi: 71
-try:
-  P8_29 = 0x39 
-except:
-  pass
+try: P8_29 = 0x39 
+except: pass
 
 # pruio_pins.bi: 73
-try:
-  P8_30 = 0x3B 
-except:
-  pass
+try: P8_30 = 0x3B 
+except: pass
 
 # pruio_pins.bi: 75
-try:
-  P8_31 = 0x36 
-except:
-  pass
+try: P8_31 = 0x36 
+except: pass
 
 # pruio_pins.bi: 77
-try:
-  P8_32 = 0x37 
-except:
-  pass
+try: P8_32 = 0x37 
+except: pass
 
 # pruio_pins.bi: 79
-try:
-  P8_33 = 0x35 
-except:
-  pass
+try: P8_33 = 0x35 
+except: pass
 
 # pruio_pins.bi: 81
-try:
-  P8_34 = 0x33 
-except:
-  pass
+try: P8_34 = 0x33 
+except: pass
 
 # pruio_pins.bi: 83
-try:
-  P8_35 = 0x34 
-except:
-  pass
+try: P8_35 = 0x34 
+except: pass
 
 # pruio_pins.bi: 85
-try:
-  P8_36 = 0x32 
-except:
-  pass
+try: P8_36 = 0x32 
+except: pass
 
 # pruio_pins.bi: 87
-try:
-  P8_37 = 0x30 
-except:
-  pass
+try: P8_37 = 0x30 
+except: pass
 
 # pruio_pins.bi: 89
-try:
-  P8_38 = 0x31 
-except:
-  pass
+try: P8_38 = 0x31 
+except: pass
 
 # pruio_pins.bi: 91
-try:
-  P8_39 = 0x2E 
-except:
-  pass
+try: P8_39 = 0x2E 
+except: pass
 
 # pruio_pins.bi: 93
-try:
-  P8_40 = 0x2F 
-except:
-  pass
+try: P8_40 = 0x2F 
+except: pass
 
 # pruio_pins.bi: 95
-try:
-  P8_41 = 0x2C 
-except:
-  pass
+try: P8_41 = 0x2C 
+except: pass
 
 # pruio_pins.bi: 97
-try:
-  P8_42 = 0x2D 
-except:
-  pass
+try: P8_42 = 0x2D 
+except: pass
 
 # pruio_pins.bi: 99
-try:
-  P8_43 = 0x2A 
-except:
-  pass
+try: P8_43 = 0x2A 
+except: pass
 
 # pruio_pins.bi: 101
-try:
-  P8_44 = 0x2B 
-except:
-  pass
+try: P8_44 = 0x2B 
+except: pass
 
 # pruio_pins.bi: 103
-try:
-  P8_45 = 0x28 
-except:
-  pass
+try: P8_45 = 0x28 
+except: pass
 
 # pruio_pins.bi: 105
-try:
-  P8_46 = 0x29 
-except:
-  pass
+try: P8_46 = 0x29 
+except: pass
 
 # pruio_pins.bi: 108
-try:
-  P9_11 = 0x1C 
-except:
-  pass
+try: P9_11 = 0x1C 
+except: pass
 
 # pruio_pins.bi: 110
-try:
-  P9_12 = 0x1E 
-except:
-  pass
+try: P9_12 = 0x1E 
+except: pass
 
 # pruio_pins.bi: 112
-try:
-  P9_13 = 0x1D 
-except:
-  pass
+try: P9_13 = 0x1D 
+except: pass
 
 # pruio_pins.bi: 114
-try:
-  P9_14 = 0x12 
-except:
-  pass
+try: P9_14 = 0x12 
+except: pass
 
 # pruio_pins.bi: 116
-try:
-  P9_15 = 0x10 
-except:
-  pass
+try: P9_15 = 0x10 
+except: pass
 
 # pruio_pins.bi: 118
-try:
-  P9_16 = 0x13 
-except:
-  pass
+try: P9_16 = 0x13 
+except: pass
 
 # pruio_pins.bi: 120
-try:
-  P9_17 = 0x57 
-except:
-  pass
+try: P9_17 = 0x57 
+except: pass
 
 # pruio_pins.bi: 122
-try:
-  P9_18 = 0x56 
-except:
-  pass
+try: P9_18 = 0x56 
+except: pass
 
 # pruio_pins.bi: 124
-try:
-  P9_19 = 0x5F 
-except:
-  pass
+try: P9_19 = 0x5F 
+except: pass
 
 # pruio_pins.bi: 126
-try:
-  P9_20 = 0x5E 
-except:
-  pass
+try: P9_20 = 0x5E 
+except: pass
 
 # pruio_pins.bi: 128
-try:
-  P9_21 = 0x55 
-except:
-  pass
+try: P9_21 = 0x55 
+except: pass
 
 # pruio_pins.bi: 130
-try:
-  P9_22 = 0x54 
-except:
-  pass
+try: P9_22 = 0x54 
+except: pass
 
 # pruio_pins.bi: 132
-try:
-  P9_23 = 0x11 
-except:
-  pass
+try: P9_23 = 0x11 
+except: pass
 
 # pruio_pins.bi: 134
-try:
-  P9_24 = 0x61 
-except:
-  pass
+try: P9_24 = 0x61 
+except: pass
 
 # pruio_pins.bi: 136
-try:
-  P9_25 = 0x6B 
-except:
-  pass
+try: P9_25 = 0x6B 
+except: pass
 
 # pruio_pins.bi: 138
-try:
-  P9_26 = 0x60 
-except:
-  pass
+try: P9_26 = 0x60 
+except: pass
 
 # pruio_pins.bi: 140
-try:
-  P9_27 = 0x69 
-except:
-  pass
+try: P9_27 = 0x69 
+except: pass
 
 # pruio_pins.bi: 142
-try:
-  P9_28 = 0x67 
-except:
-  pass
+try: P9_28 = 0x67 
+except: pass
 
 # pruio_pins.bi: 144
-try:
-  P9_29 = 0x65 
-except:
-  pass
+try: P9_29 = 0x65 
+except: pass
 
 # pruio_pins.bi: 146
-try:
-  P9_30 = 0x66 
-except:
-  pass
+try: P9_30 = 0x66 
+except: pass
 
 # pruio_pins.bi: 148
-try:
-  P9_31 = 0x64 
-except:
-  pass
+try: P9_31 = 0x64 
+except: pass
 
 # pruio_pins.bi: 150
-try:
-  P9_41 = 0x6D 
-except:
-  pass
+try: P9_41 = 0x6D 
+except: pass
 
 # pruio_pins.bi: 152
-try:
-  P9_42 = 0x59 
-except:
-  pass
+try: P9_42 = 0x59 
+except: pass
 
 # pruio_pins.bi: 155
-try:
-  JT_04 = 0x5C 
-except:
-  pass
+try: JT_04 = 0x5C 
+except: pass
 
 # pruio_pins.bi: 157
-try:
-  JT_05 = 0x5D 
-except:
-  pass
+try: JT_05 = 0x5D 
+except: pass
 
 # pruio_pins.bi: 160
-try:
-  AIN0 = 1
-except:
-  pass
+try: AIN0 = 1
+except: pass
 
 # pruio_pins.bi: 162
-try:
-  AIN1 = 2
-except:
-  pass
+try: AIN1 = 2
+except: pass
 
 # pruio_pins.bi: 164
-try:
-  AIN2 = 3
-except:
-  pass
+try: AIN2 = 3
+except: pass
 
 # pruio_pins.bi: 166
-try:
-  AIN3 = 4
-except:
-  pass
+try: AIN3 = 4
+except: pass
 
 # pruio_pins.bi: 168
-try:
-  AIN4 = 5
-except:
-  pass
+try: AIN4 = 5
+except: pass
 
 # pruio_pins.bi: 170
-try:
-  AIN5 = 6
-except:
-  pass
+try: AIN5 = 6
+except: pass
 
 # pruio_pins.bi: 172
-try:
-  AIN6 = 7
-except:
-  pass
+try: AIN6 = 7
+except: pass
 
 # pruio_pins.bi: 174
-try:
-  AIN7 = 8
-except:
-  pass
+try: AIN7 = 8
+except: pass
 
 # pruio_pins.bi: 176
-try:
-  P9_39 = AIN0
-except:
-  pass
+try: P9_39 = AIN0
+except: pass
 
 # pruio_pins.bi: 178
-try:
-  P9_40 = AIN1
-except:
-  pass
+try: P9_40 = AIN1
+except: pass
 
 # pruio_pins.bi: 180
-try:
-  P9_37 = AIN2
-except:
-  pass
+try: P9_37 = AIN2
+except: pass
 
 # pruio_pins.bi: 182
-try:
-  P9_38 = AIN3
-except:
-  pass
+try: P9_38 = AIN3
+except: pass
 
 # pruio_pins.bi: 184
-try:
-  P9_33 = AIN4
-except:
-  pass
+try: P9_33 = AIN4
+except: pass
 
 # pruio_pins.bi: 186
-try:
-  P9_36 = AIN5
-except:
-  pass
+try: P9_36 = AIN5
+except: pass
 
 # pruio_pins.bi: 188
-try:
-  P9_35 = AIN6
-except:
-  pass
+try: P9_35 = AIN6
+except: pass
 
 # pruio_pins.bi: 236
 P8_Pins = [ 
@@ -644,296 +476,201 @@ MCASP0_Pins = [
 
 
 # pruio.hp: 97
-try:
-  PRUIO_IRPT = 25
-except:
-  pass
+try: PRUIO_IRPT = 25
+except: pass
 
 # pruio.hp: 98
-try:
-  IRPT = PRUIO_IRPT + 16
-except:
-  pass
+try: IRPT = PRUIO_IRPT + 16
+except: pass
 
 # pruio.hp: 99
-try:
-  CTBIR = 0x22020
-except:
-  pass
+try: CTBIR = 0x22020
+except: pass
 
 # pruio.hp: 100
-try:
-  CONST_PRUCFG = C4
-except:
-  pass
+try: CONST_PRUCFG = C4
+except: pass
 
 # pruio.hp: 101
-try:
-  DRam = C24
-except:
-  pass
+try: DRam = C24
+except: pass
 
 # pruio.hp: 103
-try:
-  PRUIO_AZ_BALL = 109
-except:
-  pass
+try: PRUIO_AZ_BALL = 109
+except: pass
 
 # pruio.hp: 104
-try:
-  PRUIO_AZ_GPIO = 3
-except:
-  pass
+try: PRUIO_AZ_GPIO = 3
+except: pass
 
 # pruio.hp: 105
-try:
-  PRUIO_AZ_PWMSS = 2
-except:
-  pass
+try: PRUIO_AZ_PWMSS = 2
+except: pass
 
 # pruio.hp: 106
-try:
-  PRUIO_AZ_TIMER = 3
-except:
-  pass
+try: PRUIO_AZ_TIMER = 3
+except: pass
 
 # pruio.hp: 108
-try:
-  PRUIO_COM_POKE = 10
-except:
-  pass
+try: PRUIO_COM_POKE = 10
+except: pass
 
 # pruio.hp: 109
-try:
-  PRUIO_COM_PEEK = 9
-except:
-  pass
+try: PRUIO_COM_PEEK = 9
+except: pass
 
 # pruio.hp: 110
-try:
-  PRUIO_COM_GPIO_CONF = 20
-except:
-  pass
+try: PRUIO_COM_GPIO_CONF = 20
+except: pass
 
 # pruio.hp: 111
-try:
-  PRUIO_COM_GPIO_OUT = 19
-except:
-  pass
+try: PRUIO_COM_GPIO_OUT = 19
+except: pass
 
 # pruio.hp: 112
-try:
-  PRUIO_COM_PWM = 30
-except:
-  pass
+try: PRUIO_COM_PWM = 30
+except: pass
 
 # pruio.hp: 113
-try:
-  PRUIO_COM_CAP_PWM = 29
-except:
-  pass
+try: PRUIO_COM_CAP_PWM = 29
+except: pass
 
 # pruio.hp: 114
-try:
-  PRUIO_COM_CAP = 28
-except:
-  pass
+try: PRUIO_COM_CAP = 28
+except: pass
 
 # pruio.hp: 115
-try:
-  PRUIO_COM_QEP = 27
-except:
-  pass
+try: PRUIO_COM_QEP = 27
+except: pass
 
 # pruio.hp: 116
-try:
-  PRUIO_COM_TIM_PWM = 40
-except:
-  pass
+try: PRUIO_COM_TIM_PWM = 40
+except: pass
 
 # pruio.hp: 117
-try:
-  PRUIO_COM_TIM_CAP = 38
-except:
-  pass
+try: PRUIO_COM_TIM_CAP = 38
+except: pass
 
 # pruio.hp: 118
-try:
-  PRUIO_COM_ADC = 50
-except:
-  pass
+try: PRUIO_COM_ADC = 50
+except: pass
 
 # pruio.hp: 120
-try:
-  PRUIO_DAT_GPIO = 64
-except:
-  pass
+try: PRUIO_DAT_GPIO = 64
+except: pass
 
 # pruio.hp: 121
-try:
-  PRUIO_DAT_PWM = 128
-except:
-  pass
+try: PRUIO_DAT_PWM = 128
+except: pass
 
 # pruio.hp: 122
-try:
-  PRUIO_DAT_TIMER = 224
-except:
-  pass
+try: PRUIO_DAT_TIMER = 224
+except: pass
 
 # pruio.hp: 123
-try:
-  PRUIO_DAT_ADC = 288
-except:
-  pass
+try: PRUIO_DAT_ADC = 288
+except: pass
 
 # pruio.hp: 124
-try:
-  PRUIO_DAT_ALL = 512
-except:
-  pass
+try: PRUIO_DAT_ALL = 512
+except: pass
 
 # pruio.hp: 126
-try:
-  PRUIO_MSG_INIT_RUN = 4294967295
-except:
-  pass
+try: PRUIO_MSG_INIT_RUN = 4294967295
+except: pass
 
 # pruio.hp: 127
-try:
-  PRUIO_MSG_CONF_RUN = 4294967294
-except:
-  pass
+try: PRUIO_MSG_CONF_RUN = 4294967294
+except: pass
 
 # pruio.hp: 128
-try:
-  PRUIO_MSG_INIT_OK = 4294967293
-except:
-  pass
+try: PRUIO_MSG_INIT_OK = 4294967293
+except: pass
 
 # pruio.hp: 129
-try:
-  PRUIO_MSG_CONF_OK = 4294967292
-except:
-  pass
+try: PRUIO_MSG_CONF_OK = 4294967292
+except: pass
 
 # pruio.hp: 130
-try:
-  PRUIO_MSG_ADC_ERRR = 4294967291
-except:
-  pass
+try: PRUIO_MSG_ADC_ERRR = 4294967291
+except: pass
 
 # pruio.hp: 131
-try:
-  PRUIO_MSG_MM_WAIT = 4294967290
-except:
-  pass
+try: PRUIO_MSG_MM_WAIT = 4294967290
+except: pass
 
 # pruio.hp: 132
-try:
-  PRUIO_MSG_MM_TRG1 = 4294967289
-except:
-  pass
+try: PRUIO_MSG_MM_TRG1 = 4294967289
+except: pass
 
 # pruio.hp: 133
-try:
-  PRUIO_MSG_MM_TRG2 = 4294967288
-except:
-  pass
+try: PRUIO_MSG_MM_TRG2 = 4294967288
+except: pass
 
 # pruio.hp: 134
-try:
-  PRUIO_MSG_MM_TRG3 = 4294967287
-except:
-  pass
+try: PRUIO_MSG_MM_TRG3 = 4294967287
+except: pass
 
 # pruio.hp: 135
-try:
-  PRUIO_MSG_MM_TRG4 = 4294967286
-except:
-  pass
+try: PRUIO_MSG_MM_TRG4 = 4294967286
+except: pass
 
 # pruio.hp: 136
-try:
-  PRUIO_MSG_IO_OK = 4294967285
-except:
-  pass
+try: PRUIO_MSG_IO_OK = 4294967285
+except: pass
 
 
 # pruio_globals.bi: 14
-try:
-  PRUIO_DEF_AVRAGE = 4
-except:
-  pass
+try: PRUIO_DEF_AVRAGE = 4
+except: pass
 
 # pruio_globals.bi: 16
-try:
-  PRUIO_DEF_ODELAY = 183
-except:
-  pass
+try: PRUIO_DEF_ODELAY = 183
+except: pass
 
 # pruio_globals.bi: 18
-try:
-  PRUIO_DEF_SDELAY = 0
-except:
-  pass
+try: PRUIO_DEF_SDELAY = 0
+except: pass
 
 # pruio_globals.bi: 20
-try:
-  PRUIO_DEF_SAMPLS = 1
-except:
-  pass
+try: PRUIO_DEF_SAMPLS = 1
+except: pass
 
 # pruio_globals.bi: 22
-try:
-  PRUIO_DEF_STPMSK = 0b111111110
-except:
-  pass
+try: PRUIO_DEF_STPMSK = 0b111111110
+except: pass
 
 # pruio_globals.bi: 24
-try:
-  PRUIO_DEF_TIMERV = 0
-except:
-  pass
+try: PRUIO_DEF_TIMERV = 0
+except: pass
 
 # pruio_globals.bi: 26
-try:
-  PRUIO_DEF_LSLMOD = 4
-except:
-  pass
+try: PRUIO_DEF_LSLMOD = 4
+except: pass
 
 # pruio_globals.bi: 28
-try:
-  PRUIO_DEF_CLKDIV = 0
-except:
-  pass
+try: PRUIO_DEF_CLKDIV = 0
+except: pass
 
 # pruio_globals.bi: 35
-
 Int8 = c_byte
 
 # pruio_globals.bi: 36
-
 Int16 = c_short
 
 # pruio_globals.bi: 37
-
 Int32 = c_long
 
 # pruio_globals.bi: 38
-
 UInt8 = c_ubyte
 
 # pruio_globals.bi: 39
-
 UInt16 = c_ushort
 
 # pruio_globals.bi: 40
-
 UInt32 = c_ulong
 
 # pruio_globals.bi: 41
-
 Float_t = c_float
 
 # pruio_globals.bi: 43
@@ -977,46 +714,26 @@ class TimerUdt(Structure):
 TimerUdt_ = TimerUdt
 
 # pruio_globals.bi: 64
-try:
-  PRUIO_PULL_DOWN = 0b000000 
-except:
-  pass
-try:
-  PRUIO_NO_PULL = 0b001000 
-except:
-  pass
-try:
-  PRUIO_PULL_UP = 0b010000 
-except:
-  pass
-try:
-  PRUIO_RX_ACTIV = 0b100000 
-except:
-  pass
-try:
-  PRUIO_GPIO_OUT0 = 7 + PRUIO_NO_PULL                    
-except:
-  pass
-try:
-  PRUIO_GPIO_OUT1 = 7 + PRUIO_NO_PULL + 128              
-except:
-  pass
-try:
-  PRUIO_GPIO_IN = 7 + PRUIO_NO_PULL + PRUIO_RX_ACTIV   
-except:
-  pass
-try:
-  PRUIO_GPIO_IN_0 = 7 + PRUIO_PULL_DOWN + PRUIO_RX_ACTIV 
-except:
-  pass
-try:
-  PRUIO_GPIO_IN_1 = 7 + PRUIO_PULL_UP + PRUIO_RX_ACTIV   
-except:
-  pass
-try:
-  PRUIO_PIN_RESET = 0xFF
-except:
-  pass
+try: PRUIO_PULL_DOWN = 0b000000 
+except: pass
+try: PRUIO_NO_PULL = 0b001000 
+except: pass
+try: PRUIO_PULL_UP = 0b010000 
+except: pass
+try: PRUIO_RX_ACTIV = 0b100000 
+except: pass
+try: PRUIO_GPIO_OUT0 = 7 + PRUIO_NO_PULL                    
+except: pass
+try: PRUIO_GPIO_OUT1 = 7 + PRUIO_NO_PULL + 128              
+except: pass
+try: PRUIO_GPIO_IN = 7 + PRUIO_NO_PULL + PRUIO_RX_ACTIV   
+except: pass
+try: PRUIO_GPIO_IN_0 = 7 + PRUIO_PULL_DOWN + PRUIO_RX_ACTIV 
+except: pass
+try: PRUIO_GPIO_IN_1 = 7 + PRUIO_PULL_UP + PRUIO_RX_ACTIV   
+except: pass
+try: PRUIO_PIN_RESET = 0xFF
+except: pass
 
 
 # pruio_adc.bi: 29
@@ -1338,86 +1055,50 @@ TimerUdt._fields_ = [
 
 
 # pruio.bi: 36
-try:
-  PRUIO_CHAN = CHANNEL5
-except:
-  pass
+try: PRUIO_CHAN = CHANNEL5
+except: pass
 
 # pruio.bi: 38
-try:
-  PRUIO_MASK = PRU_EVTOUT5_HOSTEN_MASK
-except:
-  pass
+try: PRUIO_MASK = PRU_EVTOUT5_HOSTEN_MASK
+except: pass
 
 # pruio.bi: 40
-try:
-  PRUIO_EMAP = PRU_EVTOUT5
-except:
-  pass
+try: PRUIO_EMAP = PRU_EVTOUT5
+except: pass
 
 # pruio.bi: 42
-try:
-  PRUIO_EVNT = PRU_EVTOUT_5
-except:
-  pass
+try: PRUIO_EVNT = PRU_EVTOUT_5
+except: pass
 
 # pruio.bi: 86
-try:
-  PRUIO_ACT_PRU1 = 0b0000000000001 
-except:
-  pass
-try:
-  PRUIO_ACT_ADC = 0b0000000000010 
-except:
-  pass
-try:
-  PRUIO_ACT_GPIO0 = 0b0000000000100 
-except:
-  pass
-try:
-  PRUIO_ACT_GPIO1 = 0b0000000001000 
-except:
-  pass
-try:
-  PRUIO_ACT_GPIO2 = 0b0000000010000 
-except:
-  pass
-try:
-  PRUIO_ACT_GPIO3 = 0b0000000100000 
-except:
-  pass
-try:
-  PRUIO_ACT_PWM0 = 0b0000001000000 
-except:
-  pass
-try:
-  PRUIO_ACT_PWM1 = 0b0000010000000 
-except:
-  pass
-try:
-  PRUIO_ACT_PWM2 = 0b0000100000000 
-except:
-  pass
-try:
-  PRUIO_ACT_TIM4 = 0b0001000000000 
-except:
-  pass
-try:
-  PRUIO_ACT_TIM5 = 0b0010000000000 
-except:
-  pass
-try:
-  PRUIO_ACT_TIM6 = 0b0100000000000 
-except:
-  pass
-try:
-  PRUIO_ACT_TIM7 = 0b1000000000000 
-except:
-  pass
-try:
-  PRUIO_DEF_ACTIVE = 0b1111111111111 
-except:
-  pass
+try: PRUIO_ACT_PRU1 = 0b0000000000001 
+except: pass
+try: PRUIO_ACT_ADC = 0b0000000000010 
+except: pass
+try: PRUIO_ACT_GPIO0 = 0b0000000000100 
+except: pass
+try: PRUIO_ACT_GPIO1 = 0b0000000001000 
+except: pass
+try: PRUIO_ACT_GPIO2 = 0b0000000010000 
+except: pass
+try: PRUIO_ACT_GPIO3 = 0b0000000100000 
+except: pass
+try: PRUIO_ACT_PWM0 = 0b0000001000000 
+except: pass
+try: PRUIO_ACT_PWM1 = 0b0000010000000 
+except: pass
+try: PRUIO_ACT_PWM2 = 0b0000100000000 
+except: pass
+try: PRUIO_ACT_TIM4 = 0b0001000000000 
+except: pass
+try: PRUIO_ACT_TIM5 = 0b0010000000000 
+except: pass
+try: PRUIO_ACT_TIM6 = 0b0100000000000 
+except: pass
+try: PRUIO_ACT_TIM7 = 0b1000000000000 
+except: pass
+try: PRUIO_DEF_ACTIVE = 0b1111111111111 
+except: pass
 
 # pruio.bi: 102
 
